4233fe6d15a35dc694dc30c554315cbf553e6bfe
Makarem12 <makaremabososain@gmail.com>
2024-08-17 19:27:27 +0300

    done

---
+++ library-book-tracker/pages/index.js
@@ -0,0 +1,14 @@
+'use client';
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import Footer from "@/components/Footer";
+
+export default function page(){
+  return (
+    <>
+    <Header />
+    <Form />
+    <Footer />
+    </>
+  )
+}
